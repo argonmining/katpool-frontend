f4be0b486bce4d58098ce02411613c8c8c571d1b
argonmining <ashton@onargon.com>
2024-12-26 15:02:48 -0800

    dates

--- components/elements/dropdown-announcements.tsx
+++ components/elements/dropdown-announcements.tsx
@@ -48,7 +48,7 @@ export default function DropdownAnnouncements({ align }: {
                       🎉 <span className="font-medium text-gray-800 dark:text-gray-100">Kat Pool Open Beta is Live!</span> We're excited to announce 
                       that Kat Pool is now in Open Beta. During this testing phase, all pool fees are set to zero. Thank you for helping us test and improve our platform.
                     </span>
-                    <span className="block text-xs font-medium text-gray-400 dark:text-gray-500">Feb 12, 2024</span>
+                    <span className="block text-xs font-medium text-gray-400 dark:text-gray-500">Jan 6, 2025</span>
                   </Link>
                 )}
               </MenuItem>
@@ -59,23 +59,10 @@ export default function DropdownAnnouncements({ align }: {
                       💎 <span className="font-medium text-gray-800 dark:text-gray-100">KRC20 Payouts Coming Soon</span> Our team is actively developing 
                       KRC20 token payout functionality. Stay tuned for this exciting feature that will be available in the near future.
                     </span>
-                    <span className="block text-xs font-medium text-gray-400 dark:text-gray-500">Feb 9, 2024</span>
+                    <span className="block text-xs font-medium text-gray-400 dark:text-gray-500">Jan 1, 2025</span>
                   </Link>
                 )}
               </MenuItem>
-              {/* Third announcement commented out
-              <MenuItem as="li" className="border-b border-gray-200 dark:border-gray-700/60 last:border-0">
-                {({ active }) => (
-                  <Link className={`block py-2 px-4 ${active && 'bg-gray-50 dark:bg-gray-700/20'}`} href="#0">
-                    <span className="block text-sm mb-2">
-                      🚀<span className="font-medium text-gray-800 dark:text-gray-100">Say goodbye to paper receipts!</span> Sint occaecat cupidatat non proident, sunt
-                      in culpa qui officia deserunt mollit anim.
-                    </span>
-                    <span className="block text-xs font-medium text-gray-400 dark:text-gray-500">Jan 24, 2024</span>
-                  </Link>
-                )}
-              </MenuItem>
-              */}
             </MenuItems>
           </Transition>
         </>
