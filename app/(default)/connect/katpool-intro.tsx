4342692b46c57f45927225a054041d8b5a2ce28e
argonmining <ashton@onargon.com>
2024-12-26 21:28:55 -0800

    dep fix

--- app/(default)/connect/katpool-intro.tsx
+++ app/(default)/connect/katpool-intro.tsx
@@ -115,7 +115,7 @@ export default function KatpoolIntro() {
               className="w-full h-full"
               src="about:blank" // Replace with actual YouTube embed URL
               title="KatPool Setup Tutorial"
-              frameBorder="0"
+              style={{ border: 0 }}
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
