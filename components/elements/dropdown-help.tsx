12e1b7932ffe42ca88afbfbec4afb6b1d40acc52
argonmining <ashton@onargon.com>
2025-01-09 00:53:46 -0800

    info menu

--- components/elements/dropdown-help.tsx
+++ components/elements/dropdown-help.tsx
@@ -48,7 +48,7 @@ export default function DropdownHelp({ align }: {
                       <rect y="3" width="12" height="9" rx="1" />
                       <path d="M2 0h8v2H2z" />
                     </svg>
-                    <span>Pool Treasury Wallet</span>
+                    <span>Pool Treasury</span>
                   </Link>
                 )}
               </MenuItem>
