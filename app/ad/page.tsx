48ce0fc082bb69d29cb6e4ccda8c8744845c2451
argonmining <ashton@onargon.com>
2025-01-10 22:17:50 -0800

    gif ad add

---
+++ app/ad/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+export default function GifPage() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <img src="/katpoolad.gif" alt="Hosted GIF" />
+    </div>
+  )
+} 
