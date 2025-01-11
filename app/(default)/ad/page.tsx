77e945939bd9a2de5d307b20e012aed4d29f8624
argonmining <ashton@onargon.com>
2025-01-10 22:23:54 -0800

    ad fix

--- app/(default)/ad/page.tsx
+++ app/(default)/ad/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function GifPage() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <img src="/katpoolad.gif" alt="Hosted GIF" />
+      <img src="/images/katpoolad.gif" alt="Katpool Ad" />
     </div>
   )
 } 
