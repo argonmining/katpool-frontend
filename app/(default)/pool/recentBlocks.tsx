b3f0cd21ef3e187a84876dde93626159b71db9c1
argonmining <ashton@onargon.com>
2025-01-26 18:14:52 -0800

    same

--- app/(default)/pool/recentBlocks.tsx
+++ app/(default)/pool/recentBlocks.tsx
@@ -170,7 +170,7 @@ export default function RecentBlocks() {
                               rel="noopener noreferrer"
                             >{formatBlockHash(block.blockHash)}</a> found!
                             {/* <span className="text-gray-500 dark:text-gray-400"> • Reward: </span> */}
-                            <span className="text-green-500">{block.reward} KAS</span>
+                            {/* <span className="text-green-500">{block.reward} KAS</span> */}
                           </div>
                           <div className="shrink-0 self-end ml-2">
                             <span className="text-gray-400 dark:text-gray-500">
